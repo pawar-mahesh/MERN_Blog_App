2401effc854f95f1bf71867b166252957d123cfa
Mahesh Pawar <pawar.mahesh2345@gmail.com>
2024-05-26 13:43:05 +0530

    api: Create 'sign-in' API
    Signed-off-by: Mahesh Pawar <pawar.mahesh2345@gmail.com>

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,6 +1,7 @@
 import User from "../models/user.model.js";
 import bcryptjs from "bcryptjs";
 import { errorHandler } from "../utils/error.js";
+import jwt from "jsonwebtoken";
 
 // function for signup
 export const signup = async (req, res, next) => {
@@ -45,5 +46,57 @@ export const signup = async (req, res, next) => {
       err.message = `${duplicate} is already exist, please go to log in.`;
     }
     return next(errorHandler(400, err.message));
+  }
+};
+
+// funtion for signin
+export const signin = async (req, res, next) => {
+  const { email, password } = req.body;
+
+  // check for all the inputs
+  if (
+    (email && !email.trim()) ||
+    (password && !password.trim()) ||
+    !email ||
+    !password
+  ) {
+    return next(errorHandler(400, "All fields are required!"));
+  }
+
+  try {
+    // validate the user already exist
+    const validUser = await User.findOne({ email });
+
+    if (!validUser) {
+      return next(errorHandler(400, "No user exist, please check credentials"));
+    }
+
+    // compare the password entered by user and the already exsiting password
+    const validPassword = bcryptjs.compareSync(password, validUser.password);
+
+    if (!validPassword) {
+      return next(
+        errorHandler(400, "Wrong attempt, please check your password")
+      );
+    }
+
+    // create a jwt token using sing method which require json with unique identity and secrete key as two parameters
+    const token = jwt.sign(
+      {
+        id: validUser._id,
+      },
+      process.env.JWT_SECRET
+    );
+
+    // destruture the data to skip to password
+    const { password: pass, ...rest } = validUser._doc;
+
+    // send a response with cookies
+    res
+      .status(200)
+      .cookie("access_token", token, { httpOnly: true })
+      .json(rest);
+  } catch (err) {
+    next(err);
   }
 };
